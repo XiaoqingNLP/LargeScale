a51aa9fe17b082af5c1fd9e3591952ab80d285f2
Sengxian <sengxian@live.com>
2022-04-12 13:18:37 +0800

    Fix argument bug

--- glm/data.py
+++ glm/data.py
@@ -21,7 +21,7 @@ def build_train_valid_test_datasets(
 ):
     tokenizer = get_tokenizer()
 
-    assert args.seq_len == aggregated_samples_per_sequence * args.max_position_embeddings
+    assert args.seq_length == aggregated_samples_per_sequence * args.max_position_embeddings
 
     collator = GLMPreprocessor(
         eod_id=tokenizer.get_special_token("eod"),
