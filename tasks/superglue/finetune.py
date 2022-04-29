2a9e9b5a463314ad8eca5e74136161d1cc2a19b7
Sengxian <sengxian@live.com>
2022-04-29 10:50:20 +0800

    Implement WSC

--- tasks/superglue/finetune.py
+++ tasks/superglue/finetune.py
@@ -20,6 +20,7 @@ import os
 from collections import OrderedDict
 from megatron import get_args, get_timers, print_rank_0, get_tokenizer, mpu
 from megatron.enums import PositionEmbeddingType
+from megatron.utils import average_losses_across_data_parallel_group
 from tasks.finetune_utils import finetune
 from tasks.superglue.dataset import SuperGlueDataset, PROCESSORS, get_output_func
 from tasks.superglue.dataset import MULTI_CHOICE_DATASETS
@@ -27,6 +28,7 @@ from tasks.superglue.evaluate import qa_exact_match, qa_f1, multirc_em
 from tasks.superglue.pvp import PVPS
 from tasks.superglue.eval_utils import accuracy_metric, f1_macro_metric, f1_metric, accuracy_func_provider
 from pretrain_glm import model_provider as glm_model_provider
+from pretrain_glm import process_data as process_batch_lm
 from glm.model import GLMForSingleTokenCloze, GLMForMultiTokenClozeFast, GLMForMultiTokenCloze
 from tasks.finetune_utils import cross_entropy_loss_func
 from functools import partial
@@ -71,21 +73,26 @@ def train_valid_datasets_provider(pattern_text=False):
     return train_dataset, valid_dataset
 
 
-def model_provider(pre_process=True, post_process=True):
+def model_provider(pre_process=True, post_process=True, model_type='multiple_choice'):
     """Build the model."""
     args = get_args()
 
     print_rank_0('building GLM downstream model for {} ...'.format(
         args.task))
     model = glm_model_provider(pre_process=pre_process, post_process=post_process)
-    if args.cloze_eval:
-        if args.multi_token:
-            if args.fast_decode:
-                model = GLMForMultiTokenClozeFast(model, length_penalty=args.length_penalty)
+    if model_type == 'multiple_choice':
+        if args.cloze_eval:
+            if args.multi_token:
+                if args.fast_decode:
+                    model = GLMForMultiTokenClozeFast(model, length_penalty=args.length_penalty)
+                else:
+                    model = GLMForMultiTokenCloze(model, length_penalty=args.length_penalty)
             else:
-                model = GLMForMultiTokenCloze(model, length_penalty=args.length_penalty)
+                model = GLMForSingleTokenCloze(model, take_softmax=args.adapet)
         else:
-            model = GLMForSingleTokenCloze(model, take_softmax=args.adapet)
+           raise NotImplementedError
+    elif model_type == 'generation':
+        pass
     else:
         raise NotImplementedError
     return model
@@ -188,6 +195,7 @@ def finetune_forward_step(batch, model):
             loss_mask = data["loss_mask"]
             logits = logits * loss_mask - 10000.0 * (1.0 - loss_mask)
         if args.loss_func == "cross_entropy":
+            assert mpu.get_tensor_model_parallel_world_size() == 1
             return logits.contiguous().float(), partial(cross_entropy_loss_func, labels)
         elif args.loss_func == "hinge":
             raise NotImplementedError
@@ -210,6 +218,114 @@ def finetune_forward_step(batch, model):
     return loss
 
 
+def forward_step_lm(batch, model, eval_metric=None):
+    """Forward step."""
+    args = get_args()
+    timers = get_timers()
+    tokenizer = get_tokenizer()
+
+    # Get the batch.
+    timers('batch generator').start()
+    try:
+        data = next(batch)
+    except BaseException:
+        data = batch
+
+    if 'mask' in data:
+        data['attention_mask'] = data.pop('mask')
+    tokens, labels, loss_mask, attention_mask, position_ids = process_batch_lm(data)
+    timers('batch generator').stop()
+
+    def print_masked_text(batch_id):
+        block_position_ids = position_ids[:, 1]
+        position_ids_ = position_ids[:, 0]
+        output_tokens = []
+        sep = attention_mask[batch_id].item()
+        for i, token in enumerate(tokens[batch_id, :sep].tolist()):
+            if tokenizer is not None:
+                token = tokenizer.IdToToken(token)
+                if token.startswith('[MASK'):
+                    token = f"[{position_ids_[batch_id, i].item()}, {token}]"
+                if token.startswith('##') and len(output_tokens) > 0 and not output_tokens[-1].endswith(']'):
+                    output_tokens[-1] += token[2:]
+                else:
+                    output_tokens.append(token)
+            else:
+                output_tokens.append(str(token))
+        print(" ".join(output_tokens))
+        last_index = None
+        for i in range(sep, tokens.size(1)):
+            if tokenizer.IdToToken(tokens[batch_id, i].item()).startswith("[sop"):
+                if last_index is not None:
+                    print(tokenizer.DecodeIds(tokens[batch_id, last_index: i].tolist()), "|",
+                          tokenizer.DecodeIds(labels[batch_id, last_index: i].tolist())),
+                    print(position_ids_[batch_id, last_index: i].tolist(),
+                          block_position_ids[batch_id, last_index:i].tolist())
+                last_index = i
+        if last_index is not None:
+            print(tokenizer.DecodeIds(tokens[batch_id, last_index:].tolist()), "|",
+                  tokenizer.DecodeIds(labels[batch_id, last_index:].tolist()))
+            print(position_ids_[batch_id, last_index:].tolist(), block_position_ids[batch_id, last_index:].tolist())
+
+    # Forward model.
+    if args.continuous_prompt:
+        prompt_pos = data["prompt_pos"].long().cuda()
+        logits = model(tokens, position_ids, attention_mask, prompt_pos=prompt_pos)
+    else:
+        logits = model(tokens, position_ids, attention_mask)
+
+    if eval_metric is None or eval_metric == 'loss':
+        def cross_entropy_loss_func(labels, output_tensor, loss_mask):
+            logits = output_tensor
+
+            losses = mpu.vocab_parallel_cross_entropy(logits.contiguous().float(),
+                                                      labels)
+            loss_mask = loss_mask.view(-1)
+            # The loss is not normalized for fair comparison
+            loss = torch.sum(losses.view(-1) * loss_mask)
+
+            if eval_metric is None:
+                loss = loss / loss_mask.sum()
+
+            averaged_loss = average_losses_across_data_parallel_group([loss])
+
+            return loss, {'lm loss': averaged_loss[0]}
+
+        return logits, partial(cross_entropy_loss_func, labels, loss_mask=loss_mask)
+    elif eval_metric == 'accuracy' or eval_metric == 'classify':
+        logits = mpu.gather_from_tensor_model_parallel_region(logits)
+        outputs = torch.argmax(logits, -1)
+        correct = (outputs == labels).float()
+        correct[(1 - loss_mask).bool()] = 1
+        correct = correct.prod(-1)
+        if eval_metric == 'accuracy':
+            correct = correct.sum()
+        return correct
+    else:
+        raise NotImplementedError("Metric {} not implemented".format(eval_metric))
+
+
+def classify_evaluate_lm(model, dataloader, example_dict, args):
+    """Evaluation."""
+    # Turn on evaluation mode which disables dropout.
+    assert len(model) == 1
+    model = model[0]
+    model.eval()
+    predictions, labels, examples = [], [], []
+    with torch.no_grad():
+        # For all the batches in the dataset.
+        for iteration, batch in enumerate(dataloader):
+            # Forward evaluation.
+            output = forward_step_lm(batch, model, eval_metric='classify')
+            uid_list = batch['uid']
+            example_batch = [example_dict[uid] for uid in uid_list]
+            predictions.extend(output.long().tolist())
+            label = batch['label'].tolist()
+            labels.extend(label)
+            examples.extend(example_batch)
+    return predictions, labels, examples
+
+
 def metrics_func_provider(is_test=False):
     """Privde metrics callback function."""
     args = get_args()
@@ -221,8 +337,7 @@ def metrics_func_provider(is_test=False):
     output_func = get_output_func(args.task.lower(), args)
     eval_func = None
     if args.task.lower() in ['wsc', 'squad'] and args.cloze_eval and not args.wsc_negative:
-        from tasks.language_model.finetune import classify_evaluate
-        eval_func = classify_evaluate
+        eval_func = classify_evaluate_lm
     metric_dict = OrderedDict(DEFAULT_METRICS[args.task.lower()])
     return accuracy_func_provider(single_dataset_provider, metric_dict, args, is_test=is_test, eval_func=eval_func,
                                   output_func=output_func, only_rank0=False, tokenizer=tokenizer)
@@ -259,10 +374,9 @@ def main():
                                   num_prompt_tokens=args.num_prompt_tokens)
 
     if args.task.lower() in ['wsc', 'squad'] and args.cloze_eval and not args.wsc_negative:
-        # from tasks.language_model.finetune import lm_forward_step
-        # finetune(args, train_valid_datasets_provider, model_kwargs,
-        #          end_of_epoch_callback_provider=metrics_func_provider, forward_step=lm_forward_step)
-        raise NotImplementedError
+        finetune(train_valid_datasets_provider, partial(model_provider),
+                 forward_step=forward_step_lm,
+                 end_of_epoch_callback_provider=metrics_func_provider)
     else:
         if args.cloze_eval:
             multi_token = pvp.is_multi_token
