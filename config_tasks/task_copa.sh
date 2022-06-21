285435ddceecc950cf70b379456d18f7122e013c
duzx16 <zx-du20@mails.tsinghua.edu.cn>
2022-06-21 10:10:27 +0800

    Update task config

--- config_tasks/task_copa.sh
+++ config_tasks/task_copa.sh
@@ -3,21 +3,21 @@ EXP_NAME=copa-${NAME}-${TIMESTAMP}
 DATA_PATH="${DATA_ROOT}/COPA"
 MAX_SEQ_LEN=256
 
-LR_SINGLE=1e-5
-EPOCH_SINGLE=50
+LR_SINGLE=3e-3
+EPOCH_SINGLE=100
 XXLARGE_EPOCH=100
 
 TRAIN_ARGS="--lr-decay-style linear \
             --lr-warmup-fraction 0.1 \
-            --weight-decay 1.0e-1 \
+            --weight-decay 1.0e-4 \
             --pattern-id 0"
 
-COMMON_ARGS="--save-interval 10000 \
+COMMON_ARGS="--save-interval 100000 \
              --log-interval 20 \
-             --eval-interval 1000 \
+             --eval-interval 10000000 \
              --eval-iters 100"
 
 PATTERN_IDS=(0 1)
 PROMPT_IDS=(1 2)
 
-BATCH_SIZE=16
+BATCH_SIZE=8
--- config_tasks/task_rte.sh
+++ config_tasks/task_rte.sh
@@ -3,18 +3,18 @@ EXP_NAME=rte-${NAME}-${TIMESTAMP}
 DATA_PATH="${DATA_ROOT}/RTE"
 MAX_SEQ_LEN=256
 
-LR_SINGLE=2e-5
-EPOCH_SINGLE=30
+LR_SINGLE=3e-3
+EPOCH_SINGLE=50
 XXLARGE_EPOCH=50
 
 TRAIN_ARGS="
             --lr-decay-style linear \
             --lr-warmup-fraction 0.1 \
-            --weight-decay 1.0e-1 \
+            --weight-decay 1.0e-4 \
             --pattern-id 0"
 
-COMMON_ARGS="--save-interval 10000 \
-             --log-interval 50000 \
+COMMON_ARGS="--save-interval 100000 \
+             --log-interval 20 \
              --eval-interval 10000000 \
              --eval-iters 100"
 
