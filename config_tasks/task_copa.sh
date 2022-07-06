c0f04470ad2cfe052279d9600c578e82530db815
Sengxian <sengxian@live.com>
2022-07-06 21:20:59 +0800

    Add config

--- config_tasks/task_copa.sh
+++ config_tasks/task_copa.sh
@@ -3,18 +3,18 @@ EXP_NAME=copa-${NAME}-${TIMESTAMP}
 DATA_PATH="${DATA_ROOT}/COPA"
 MAX_SEQ_LEN=256
 
-LR_SINGLE=1e-5
-EPOCH_SINGLE=50
+LR_SINGLE=8e-3
+EPOCH_SINGLE=100
 XXLARGE_EPOCH=100
 
 TRAIN_ARGS="--lr-decay-style linear \
             --lr-warmup-fraction 0.1 \
-            --weight-decay 1.0e-1 \
+            --weight-decay 1.0e-4 \
             --pattern-id 0"
 
-COMMON_ARGS="--save-interval 10000 \
-             --log-interval 20 \
-             --eval-interval 1000 \
+COMMON_ARGS="--save-interval 1000000 \
+             --log-interval 100 \
+             --eval-interval 1000000 \
              --eval-iters 100 \
              --fast-decode \
              --tgt-seq-length 32"
@@ -22,4 +22,4 @@ COMMON_ARGS="--save-interval 10000 \
 PATTERN_IDS=(0 1)
 PROMPT_IDS=(1 2)
 
-BATCH_SIZE=16
+BATCH_SIZE=8
