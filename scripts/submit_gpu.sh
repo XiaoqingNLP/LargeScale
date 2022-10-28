0871227c153af7d8bb33c1dbce5895cdb1d9f102
Aohan Zeng <Sengxian@live.com>
2022-10-28 09:32:49 +0800

    Update submit_gpu.sh

--- scripts/submit_gpu.sh
+++ scripts/submit_gpu.sh
@@ -2,7 +2,7 @@
 
 NUM_WORKERS=96
 NUM_GPUS_PER_WORKER=8
-HOST_FILE_PATH="/thudm/LargeScale/wudao-130B"
+HOST_FILE_PATH="<your hostfile path>"
 OPTIONS_NCCL="NCCL_DEBUG=VERSION NCCL_IB_DISABLE=0 NCCL_NET_GDR_LEVEL=2 CUDA_LAUNCH_BLOCKING=0"
 
 TIMESTAMP=$(date +'%Y.%m.%d-%H:%M:%S')
@@ -10,6 +10,6 @@ TIMESTAMP=$(date +'%Y.%m.%d-%H:%M:%S')
 source $1
 mkdir -p logs/${EXP_NAME}
 
-run_cmd="PYTHONPATH=/thudm/LargeScale/packages ${OPTIONS_NCCL} deepspeed --num_nodes ${NUM_WORKERS} --num_gpus ${NUM_GPUS_PER_WORKER} --hostfile ${HOST_FILE_PATH} ${script_path} ${gpt_options}"
+run_cmd="${OPTIONS_NCCL} deepspeed --num_nodes ${NUM_WORKERS} --num_gpus ${NUM_GPUS_PER_WORKER} --hostfile ${HOST_FILE_PATH} ${script_path} ${gpt_options}"
 echo ${run_cmd}
 eval ${run_cmd} 2>&1 | tee logs/${EXP_NAME}/output.log
